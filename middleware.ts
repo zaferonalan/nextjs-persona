2c082affeed73179bf9fea788c23a126e30c2f8e
Zafer Günay Önalan <zafergunay7@gmail.com>
2026-06-17 11:40:22 +0300

    add_clerckMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
@@ -8,5 +8,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
   ],
-};
+}
